c2d5ba67613792b81572d8eb5813a73b0d25faef
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-06-28 17:55:33 +0100

    Update Scanner01.java

--- src/main/java/day03wrapperclasstypeecastingmethodcreation/Scanner01.java
+++ src/main/java/day03wrapperclasstypeecastingmethodcreation/Scanner01.java
@@ -7,11 +7,10 @@ public class Scanner01 {
     public static void main(String[] args) {
 
         //--------------- How to create an Object --------------------
-
         //Scanner Object
         Scanner subscriptionForm = new Scanner(System.in);
-
         System.out.println("hey user, enter your first name...");
+        
         String yourName = subscriptionForm.next();
         System.out.println("Your name is " + yourName);
     }
