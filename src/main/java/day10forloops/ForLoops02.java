8d1be8f26cdf1e67cc35be79cd9350165589ab71
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-03 18:06:23 +0100

    version01

--- src/main/java/day10forloops/ForLoops02.java
+++ src/main/java/day10forloops/ForLoops02.java
@@ -2,13 +2,17 @@ package day10forloops;
 
 public class ForLoops02 {
     public static void main(String[] args) {
-        //Example 1: Put "*" between 2 consecutive characters and to the end in a String.
-        // For example; Java ==> J*a*v*a*
+               
+        //----------------------- For - Loops -------------------------------
+
+        //Example 1:     Put "*" between 2 consecutive characters and to the end in a String.
+        //             For example -> Java ==> J*a*v*a*
         String s = "Java";
         for (int i = 0; i < s.length(); i++) {
             System.out.print(s.charAt(i) + "*");
         }
         System.out.println();
+        
         //Example 2: Type code to print unique characters in a String. Hello ==> Heo INTERVIEW QUESTION
         String t = "Hello"; // index of the fist character is 0 so int i=0, ending point is i is less than length
         for(int i=0; i<t.length(); i++){
