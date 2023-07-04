f3f5615b47f3bcd345413ebea8d6eb298fb84161
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-04 10:08:13 +0100

    version01

--- src/main/java/day11forloop/Forloop01.java
+++ src/main/java/day11forloop/Forloop01.java
@@ -2,13 +2,17 @@ package day11forloop;
 
 public class Forloop01 {
     public static void main(String[] args) {
-        //e1. type code to find the sum of integers from 5 to 26
+        
+        //------------------------ For - Loops --------------------------
+
+        //Example 1: Type code to find the sum of integers from 5 to 26
         int sum = 0;
         for (int i = 5; i < 27; i++) {
             sum = sum + i;
         }
         System.out.println(sum);
-        //e.2. type code to find the multiplication of integers from 5 to 9
+        
+        //Example 2: Type code to find the multiplication of integers from 5 to 9
         //important
 
         int multiply = 1;
@@ -17,10 +21,9 @@ public class Forloop01 {
         }
         System.out.println(multiply); //15120
 
-
-        //e3. type code to find the sum of the digits of an integer
+        //Example 3: Type code to find the sum of the digits of an integer
         //587 == 5+8+7=20   INTERVIEW QUESTION
-        // ------- modulus %10 always gives the last digit -----------------!!!!!!!!!!!!!!!
+        // ------- modulus %10 always gives the last digit -----------------
         int num = -675;
         num = Math.abs(num); //converts negative number to positive
         int sumOfDigits = 0;
