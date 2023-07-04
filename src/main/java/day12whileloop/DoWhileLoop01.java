e55b5944681d03a415a2a5b0c6a6408249fd2b1d
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-04 10:12:43 +0100

    version01

--- src/main/java/day12whileloop/DoWhileLoop01.java
+++ src/main/java/day12whileloop/DoWhileLoop01.java
@@ -4,21 +4,26 @@ import java.util.Scanner;
 
 public class DoWhileLoop01 {
     public static void main(String[] args) {
+                
+        //------------------------ While - Loops --------------------------
 
-        //while loop checks the condition first then executes the loop body
-        // if the condition is false loop body will not be executed even once.
-        // I mean zero execution is possible in while-loop
+        /*
+            while loop checks the condition first then executes the loop body
+            if the condition is false loop body will not be executed even once.
+            I mean zero execution is possible in while-loop
+    
+            do-while loop execute the loop body then checks the condition
+            if the condition is false loop body will  be executed even once.
+            I mean zero execution is impossible in do-while-loop
+        */
 
-        //do-while loop execute the loop body then checks the condition
-        // if the condition is false loop body will  be executed even once.
-        // I mean zero execution is impossible in do-while-loop
-
-        //while loop
+        //While loop
         int i = 1;
         while (i < 1) {
             System.out.println("Im while loop"); // will not print
             i++;
         }
+        
         //do-while loop
         int k = 1;
         do {
@@ -27,7 +32,7 @@ public class DoWhileLoop01 {
         } while (k < 1);
 
 
-        //e1. ask user to enter an integer
+        //Example 1: Ask user to enter an integer
         //if the integer is less thatn 100, tell user "Won"
         //otherwise, tell user "lost"
         Scanner input = new Scanner(System.in);
