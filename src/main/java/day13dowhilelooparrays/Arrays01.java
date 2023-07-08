811edc90c1adc3d471904cc06a3100ed10f92b82
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-08 10:24:15 +0100

    version01

--- src/main/java/day13dowhilelooparrays/Arrays01.java
+++ src/main/java/day13dowhilelooparrays/Arrays01.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 
 public class Arrays01 {
     public static void main(String[] args) {
-        /*
-        1. Arrays are for storing multiple data in same data type.
-         */
+        //---------------------- Arrays -----------------------------
+  
+        //1. Arrays are for storing multiple data in same data type.
         String name = "Tom";
         // how to create an Array
         String stdNames[] = new String[5];
 
-        //how to add elements into an array
+        //How to add elements into an array
         stdNames[2] = "Tom";
         stdNames[4] = "Alex";
         stdNames[0] = "Mary";
@@ -19,14 +19,13 @@ public class Arrays01 {
         stdNames[3] = "john";
         System.out.println(Arrays.toString(stdNames));
 
-        //how to get a specific element from an array
+        //How to get a specific element from an array
         System.out.println(stdNames[4]); //Alex
 
-        //how to get number of elements in an array
+        //How to get number of elements in an array
         System.out.println(stdNames.length); // it will return 5
 
-        //e1. find the total number of characters used in stdNames array elements
-
+        //Exercise 1: find the total number of characters used in stdNames array elements
         //1.Way
         int total = 0; // create container for a new data
         for (int i = 0; i < stdNames.length; i++) {
@@ -36,7 +35,7 @@ public class Arrays01 {
         System.out.println(total);
 
 
-        //2.way: for each loop == enhanced loop
+        //2.Way: for each loop == enhanced loop
         int sum = 0;
         // [Mary, Jim, Tom, John, Alex]
         for (String w : stdNames) { // w== string name
