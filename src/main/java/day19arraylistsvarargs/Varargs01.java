2de78092ef43c9f3fa9aa55ed47685ae7912da0b
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-10 08:44:15 +0100

    version01

--- src/main/java/day19arraylistsvarargs/Varargs01.java
+++ src/main/java/day19arraylistsvarargs/Varargs01.java
@@ -2,18 +2,23 @@ package day19arraylistsvarargs;
 
 public class Varargs01 {
     public static void main(String[] args) {
+        
+   //---------------------- Varargs ---------------------------
+
         System.out.println(add(3, 5));
         System.out.println(add(3, 5, 5, 55, 3));
     }
 
     //create a method that adds two integers
-    // 1. putting a parameter after a varargs is a mistake!
-    // because varargs is infinate.
-    //2. putting a parameter before a varargs is ok!
-    //3. putting multiple varargs as parameter is a mistake
-    // note: varargs must be used only once at the end
-    //container used in method paranthesis are called parameters
-    // actual data used in method paranthesis are called argument
+    /*
+        1. putting a parameter after a varargs is a mistake!
+            because varargs is infinate.
+        2. putting a parameter before a varargs is ok!
+        3. putting multiple varargs as parameter is a mistake
+           note: varargs must be used only once at the end
+           container used in method paranthesis are called parameters
+           actual data used in method paranthesis are called argument
+    */
        public static int add(int... v){
         int sum =0;
         for(int w: v){
