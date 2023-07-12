da062d419b69cc45c7e0d7b667ef749d891a1d6b
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-12 11:30:18 +0100

    version01

--- src/main/java/day22stringbuilderstringbuffer/Constructors.java
+++ src/main/java/day22stringbuilderstringbuffer/Constructors.java
@@ -1,15 +1,15 @@
 package day22stringbuilderstringbuffer;
 
 public class Constructors {
-
+//--------------------- Constructors ---------------------------
     /*
-    1. Constructors are for creating object from a class
-    2. Java puts constructor behind every class per default, it is invisible and called as default constructor
-    Default constructors are like this public Constructors(){}
-    if you type any constructor manually, Java removes default constructor
-    3. if you want you can create your own constructors
-    4. Methods have return type but Constructors do not have return type
-    5. Methods can use any name, Constructors must use Class name as name
+        1. Constructors are for creating object from a class
+        2. Java puts constructor behind every class per default, it is invisible and called as default constructor
+        Default constructors are like this public Constructors(){}
+        if you type any constructor manually, Java removes default constructor
+        3. if you want you can create your own constructors
+        4. Methods have return type but Constructors do not have return type
+        5. Methods can use any name, Constructors must use Class name as name
      */
 
     public String stdName ="Tom";
