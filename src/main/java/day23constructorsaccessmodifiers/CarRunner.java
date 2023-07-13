83740e9890e337cc2a4e479263031c7732c75f85
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-13 07:57:40 +0100

    version01

--- src/main/java/day23constructorsaccessmodifiers/CarRunner.java
+++ src/main/java/day23constructorsaccessmodifiers/CarRunner.java
@@ -2,6 +2,7 @@ package day23constructorsaccessmodifiers;
 
 public class CarRunner {
     public static void main(String[] args) {
+//--------------------- Constructors --------------------
 
         Car car1 = new Car();
         System.out.println(car1.model);//null
