e3d5f70c21a08bf677713f71ce9a86cf1891368c
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-14 14:12:22 +0100

    version01

--- src/main/java/day23constructorsaccessmodifiers/StudentRunner.java
+++ src/main/java/day23constructorsaccessmodifiers/StudentRunner.java
@@ -1,6 +1,7 @@
 package day23constructorsaccessmodifiers;
 
 public class StudentRunner {
+    //------------- Student Runner ------------
     public static void main(String[] args) {
     Student std1 = new Student();
     }
