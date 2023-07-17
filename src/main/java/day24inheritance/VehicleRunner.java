ed25577b2acb66f53b78be5910cc313710097f4b
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-17 09:52:38 +0100

    version01

--- src/main/java/day24inheritance/VehicleRunner.java
+++ src/main/java/day24inheritance/VehicleRunner.java
@@ -1,6 +1,8 @@
 package day24inheritance;
 
 public class VehicleRunner {
+    //---------------------- Inheritance -------------------------
+
     public static void main(String[] args) {
         Civic myCivic = new Civic(5);
 
