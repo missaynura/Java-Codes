8d8cc131c4b982a059c4e6642f7a911b604ba98e
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-18 08:45:51 +0100

    version01

--- src/main/java/day24inheritancedt/Cat.java
+++ src/main/java/day24inheritancedt/Cat.java
@@ -1,6 +1,8 @@
 package day24inheritancedt;
 
 public class Cat extends Animal {
+   //---------------- Inheritance --------------------
+
     public void meow(){
         System.out.println("Cats meow...");
     }
