a3c210abea1971fad7dc32f6d7f7cf575a5bcd92
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-18 08:47:01 +0100

    version01

--- src/main/java/day24inheritancedt/Dog.java
+++ src/main/java/day24inheritancedt/Dog.java
@@ -1,6 +1,7 @@
 package day24inheritancedt;
 
 public class Dog extends Animal{
+    //---------------- Inheritance --------------------
 
     public void bark(){
         System.out.println("Dogs bark...");
