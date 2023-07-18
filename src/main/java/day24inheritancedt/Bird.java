ca1a95dd694674149b5c5aef43ddb2981d444ed7
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-18 08:45:15 +0100

    version01

--- src/main/java/day24inheritancedt/Bird.java
+++ src/main/java/day24inheritancedt/Bird.java
@@ -1,6 +1,8 @@
 package day24inheritancedt;
 
 public class Bird extends Animal {
+   //---------------- Inheritance --------------------
+
     public void tweet(){
         System.out.println("Birds tweet...");
     }
