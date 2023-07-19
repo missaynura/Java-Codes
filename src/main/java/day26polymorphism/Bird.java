b47c73614a2a0c903c8d662539f9fbdea8b57ff4
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-19 08:41:36 +0100

    version01

--- src/main/java/day26polymorphism/Bird.java
+++ src/main/java/day26polymorphism/Bird.java
@@ -1,11 +1,11 @@
 package day26polymorphism;
 
 public class Bird extends Animal {
+  //-------------------- Polymorphism -----------------------
+
     /*
     - MOR
     - if the parent class has a wider access m. ==> cannot to be narrower
     - e.g. parent class is default then child class can be ==> default, protecte and public
-
-
      */
 }
