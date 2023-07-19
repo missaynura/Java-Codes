1901ee8b0d6f54a5da78befd961c877dd6e69490
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-19 08:42:40 +0100

    version01

--- src/main/java/day26polymorphism/Mammal.java
+++ src/main/java/day26polymorphism/Mammal.java
@@ -1,7 +1,7 @@
 package day26polymorphism;
 
 public class Mammal extends Animal{
-
+    //-------------------- Polymorphism -----------------------
     public void feedWithMilk(){
         System.out.println("Feed with milk...");
     }
