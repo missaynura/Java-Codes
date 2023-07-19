6348aeee8397350d44503b2bfe59cf5908d82478
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-19 08:41:12 +0100

    version01

--- src/main/java/day26polymorphism/AnimalRunner.java
+++ src/main/java/day26polymorphism/AnimalRunner.java
@@ -1,6 +1,8 @@
 package day26polymorphism;
 
 public class AnimalRunner {
+//-------------------- Polymorphism -----------------------
+
     public static void main(String[] args) {
         Cat myCat = new Cat();
         myCat.eat();
