84438c5a81dc77565047145cb65a17fe9224e2bc
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-20 08:36:29 +0100

    version01

--- src/main/java/day27encapsulationabstraction/SeventhGraders.java
+++ src/main/java/day27encapsulationabstraction/SeventhGraders.java
@@ -1,6 +1,8 @@
 package day27encapsulationabstraction;
 
 public class SeventhGraders extends Courses {
+    //---------------------- Encapsulation and Abstraction ---------------------------
+
     @Override
     public void math() {
         System.out.println("Learn square root");
