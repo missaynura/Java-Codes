724c711fa4f0d6af8d1a1f9f17f265525f44f2a6
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-20 08:37:07 +0100

    version01

--- src/main/java/day27encapsulationabstraction/SixthGragers.java
+++ src/main/java/day27encapsulationabstraction/SixthGragers.java
@@ -1,6 +1,8 @@
 package day27encapsulationabstraction;
 
 public class SixthGragers extends Courses{
+    //---------------------- Encapsulation and Abstraction ---------------------------
+
     @Override
     public void math() {
         System.out.println("Learn +, -, :, x");
