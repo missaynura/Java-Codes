dd4353d4e72c3e32457143d51ea9b0a41d6aca8e
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-20 08:36:06 +0100

    version01

--- src/main/java/day27encapsulationabstraction/Labs.java
+++ src/main/java/day27encapsulationabstraction/Labs.java
@@ -1,6 +1,8 @@
 package day27encapsulationabstraction;
 
 public abstract class Labs extends Courses {
+    //---------------------- Encapsulation and Abstraction ---------------------------
+
     public abstract void experiment();
 
 }
