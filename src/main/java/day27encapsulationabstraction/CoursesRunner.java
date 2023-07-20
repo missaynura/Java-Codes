955974ca1f45b1c423093b646192b16e43850c03
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-20 08:35:30 +0100

    version01

--- src/main/java/day27encapsulationabstraction/CoursesRunner.java
+++ src/main/java/day27encapsulationabstraction/CoursesRunner.java
@@ -1,6 +1,8 @@
 package day27encapsulationabstraction;
 
 public class CoursesRunner {
+    //---------------------- Encapsulation and Abstraction ---------------------------
+
     public static void main(String[] args) {
 
     }
