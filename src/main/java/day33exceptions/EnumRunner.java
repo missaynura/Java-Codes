a327a30e45f7a06503411fb18efd1ee927dad9b3
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-26 09:40:19 +0100

    version01

--- src/main/java/day33exceptions/EnumRunner.java
+++ src/main/java/day33exceptions/EnumRunner.java
@@ -2,6 +2,7 @@ package day33exceptions;
 
 public class EnumRunner {
     public static void main(String[] args) {
+       //-------------------------- Exceptions ------------------------------
         System.out.println(UsStates.UTAH.getCapital());
         System.out.println(UsStates.WASHINGTON.getAbbreviation());
         System.out.println(UsStates.ALABAMA.getStateName());
