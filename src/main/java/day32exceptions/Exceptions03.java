c1160d821e053dfb7208defe09fb4c2acf416ca5
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-26 09:38:57 +0100

    version01

--- src/main/java/day32exceptions/Exceptions03.java
+++ src/main/java/day32exceptions/Exceptions03.java
@@ -1,13 +1,13 @@
 package day32exceptions;
 
 public class Exceptions03 {
+    //-------------------------- Exceptions ------------------------------
     /*
         1)We sometimes need to throw Exception for specific scenarios.
           To do that, use "throw" keyword in method body together with "new" keyword and "Exception Class Constructor"
         2)If you want to add message to your Exception type it inside the "Exception Class Constructor" parenthesis
           Like; throw new IllegalArgumentException("Age cannot be negative");
      */
-
 
     public static void main(String[] args) {
         printAge(12);//12
@@ -16,7 +16,7 @@ public class Exceptions03 {
         printMathGrade(120);
     }
 
-    //E1. create a method prints ages and throws Exception for negative ages.
+    //Example 1: create a method prints ages and throws Exception for negative ages.
     public static void printAge(int age){
         if(age<0){
             throw new IllegalArgumentException("Age cannot be negative");
@@ -24,7 +24,7 @@ public class Exceptions03 {
         System.out.println("your age is " + age);
     }
 
-    //E1. create a method prints student's math grades and throws Exception for
+    //Example 2: create a method prints student's math grades and throws Exception for
     // the values less than 0 and greater than 100
     public static void printMathGrade(int grade){
         if(grade<0 || grade>100){
@@ -32,5 +32,4 @@ public class Exceptions03 {
         }
         System.out.println("your math grade is " + grade);
     }
-
 }
