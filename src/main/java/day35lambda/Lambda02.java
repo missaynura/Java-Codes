b971e061bd3c43d3a7a5b36f300a3ad97d024724
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-07-27 09:27:18 +0100

    version01

--- src/main/java/day35lambda/Lambda02.java
+++ src/main/java/day35lambda/Lambda02.java
@@ -5,8 +5,8 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Lambda02 {
-
     public static void main(String[] args) {
+       //------------------------- Lambda ---------------------------
         List<String> names = new ArrayList<>();
         names.add("Tom");
         names.add("Alex");
@@ -44,7 +44,7 @@ public class Lambda02 {
                 forEach(Utils::printInSameLineWithSpace);
     }
 
-    //Example 2: Print elements on the console by ordering them according to their length
+    //Example 3: Print elements on the console by ordering them according to their length
     //in lower cases and un-repeatedly. If some elements are in the same length put them in alphabetical order
     public static void printDistinctSortedByLengthThenAlphabeticalLowerCase(List<String> names){
         names.
