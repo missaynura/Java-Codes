72d80570bd77a56ec4baa00adf9fb0686ec88c17
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-08-04 12:15:30 +0100

    version01

--- src/main/java/miniproject03/Bus.java
+++ src/main/java/miniproject03/Bus.java
@@ -4,19 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bus {
+    //--------------------------- Mini Project: Bus ---------------------
     //plate number, seat numbers
     public String plateNumber;
     public List<String> seats=new ArrayList<>();
 
     //when we create a Bus object plateNumber and seatNo should be present
-
     public Bus(String plateNumber){
         this.plateNumber=plateNumber;
         for(int i=1; i<33; i++){
-            this.seats.add(i+"");// concatinate or use String.valueOf() to convert int to string
+            this.seats.add(i+"");// concatenate or use String.valueOf() to convert int to string
 
         }
     }
-
-
 }
