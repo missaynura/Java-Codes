8f428aafa2e6055b3cd2560b066ed77cd74953fe
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-08-07 10:07:18 +0100

    version01

--- src/main/java/oldclass/day02typecasting_scanner/TypeCasting01.java
+++ src/main/java/oldclass/day02typecasting_scanner/TypeCasting01.java
@@ -2,50 +2,48 @@ package oldclass.day02typecasting_scanner;
 
 public class TypeCasting01 {
     public static void main(String[] args) {
+        //------------------------ Data Types ---------------------------------    
         /*
-        There are 3 type sof data types in Java
-        1. Primitive data types numeric = byte < short < int < long < float < double
-        Is limited to objects, have values but no methods. It uses a stack memory.
+            There are 3 type sof data types in Java
+            1. Primitive data types numeric = byte < short < int < long < float < double
+            Is limited to objects, have values but no methods. It uses a stack memory.
+    
+            boolean = true or false
+            char = for a single data
+    
+            2. Non-Primitive data types= String = used for multiple different characters storage.
+            it has values and methods. It uses heap memory.
+    
+            String str = "tom hanks 123";
+    
+            Primitive vs. non-primitive data types:
+            1. primitive d.types have just values, non primitive d.types
+            have values and methods.
+            2. primitive d.types are created by Java, we cannot create these.
+            But we can create non-primitive d.types whenever we need.
 
-        boolean = true or false
-        char = for a single data
-
-        2. Non-Primitive data types= String = used for multiple different characters storage.
-        it has values and methods. It uses heap memory.
-
-        String str = "tom hanks 123";
-
-        Primitive vs. non-primitive data types:
-        1. primitive d.types have just values, non primitive d.types
-        have values and methods.
-        2. primitive d.types are created by Java, we cannot create these.
-        But we can create non-primitive d.types whenever we need.
-
-        3. primitive data types use lowercase in naming convenstion.
-        non-primitive d.types use upper case.
-        4. usage of memory for primitive d.types changes from data type to data type.
-        all non-primitive data types use same amount of memory in heap memory.
-
-        There are 2 types of memory types in Java.
-        1. Stack memory - 1. primitive data and 2. references (addresses) of non-primtive
-        d.types are stored.
-
-        2. Heap memory - stores non-primitive d.types/values.
-
+            3. primitive data types use lowercase in naming convenstion.
+            non-primitive d.types use upper case.
+            4. usage of memory for primitive d.types changes from data type to data type.
+            all non-primitive data types use same amount of memory in heap memory.
+    
+            There are 2 types of memory types in Java.
+            1. Stack memory - 1. primitive data and 2. references (addresses) of non-primtive
+            d.types are stored.
+    
+            2. Heap memory - stores non-primitive d.types/values.
         */
-
 
         int j = 23, i = 12;
         String str = "Tom";
         System.out.println(j + i + str);
 
         /*
-        Example1: create 2 int variables and 1 string variable.
-        Primitive data types are created by Java, that dont have methods,
-        = byte< short <int < long < float <double -- char and boolean
-        non-primitive data types have both values and methods, so you
-        can create your own data type in non-primitive d.type.
-
+            Example1: create 2 int variables and 1 string variable.
+            Primitive data types are created by Java, that dont have methods,
+            = byte< short <int < long < float <double -- char and boolean
+            non-primitive data types have both values and methods, so you
+            can create your own data type in non-primitive d.type.
          */
 
     int a=12;
@@ -55,19 +53,17 @@ public class TypeCasting01 {
         System.out.println(name + a + b);
         System.out.println(name + (a + b));
 
-
-
         /*
-        Type Casting
-        1. AutoWidening: Java can put small data types into larger d.types
-        automatically. when you put small d.type into a larger d.type, you are
-        widening the small d.type.
-
-        Java does not put large d.type into to small d.type
-
-        2. Explicitly Narrowing: Java does not put large d. types into small d.types
-        automatically. Java thinks this is risky. To spped up an app we sometimes need
-        to put larger d.t. into smaller ones.
+            Type Casting
+            1. AutoWidening: Java can put small data types into larger d.types
+            automatically. when you put small d.type into a larger d.type, you are
+            widening the small d.type.
+    
+            Java does not put large d.type into to small d.type
+    
+            2. Explicitly Narrowing: Java does not put large d. types into small d.types
+            automatically. Java thinks this is risky. To spped up an app we sometimes need
+            to put larger d.t. into smaller ones.
          */
 
     int m= 45;
@@ -78,9 +74,5 @@ public class TypeCasting01 {
 
     int n = 15;
     byte b1= (byte) n;
-
-
-
-
     }
 }
