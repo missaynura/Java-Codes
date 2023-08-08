dc9a0cb6a8a764c0baf71e31ec845f6fe29614bb
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-08-08 11:54:14 +0100

    version01

--- src/main/java/oldclass/day03nonprimitivesscannerclass/Scanner01.java
+++ src/main/java/oldclass/day03nonprimitivesscannerclass/Scanner01.java
@@ -3,15 +3,16 @@ package oldclass.day03nonprimitivesscannerclass;
 import java.util.Scanner;
 
 public class Scanner01 {
+    //----------------- Scanner Class ---------------------
     // to get data from user in Java, we use scanner class.
     public static void main(String[] args) {
 
     /*
-    steps to get data from user
-    1. create a scanner class object
-    class name    object name   assignment operator    new    constructor
-    2. give a message to user
-    3. get the data from user by using input object
+        steps to get data from user
+        1. create a scanner class object
+        class name    object name   assignment operator    new    constructor
+        2. give a message to user
+        3. get the data from user by using input object
      */
 
         Scanner input = new Scanner(System.in);
