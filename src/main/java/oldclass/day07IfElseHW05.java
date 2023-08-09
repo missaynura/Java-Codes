8b7cb5a2ffe07d39b6ab361c538b0daf86d8e4e8
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-08-09 10:41:03 +0100

    version01

--- src/main/java/oldclass/day07IfElseHW05.java
+++ src/main/java/oldclass/day07IfElseHW05.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class day07IfElseHW05 {
     public static void main(String[] args) {
-        //ex.7: Type java code by using if-else statement.
+        //---------------- Practice ------------------------
+        //Excercise 7: Type java code by using if-else statement.
         //A shop will give discount of 10% if the cost of purchased quantity is more than 1000.
         //Ask user for quantity and unit price then judge and print total cost for user.
         //If the quantity is less than 1000 output will be “No discount.”
