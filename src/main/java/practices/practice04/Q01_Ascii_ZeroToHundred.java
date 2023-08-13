d9fb8b68e04c119766f89f081fc8ceba67981a2c
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-08-13 11:51:58 +0100

    version01

--- src/main/java/practices/practice04/Q01_Ascii_ZeroToHundred.java
+++ src/main/java/practices/practice04/Q01_Ascii_ZeroToHundred.java
@@ -1,9 +1,10 @@
 package practices.practice04;
 
 public class Q01_Ascii_ZeroToHundred {
+ //--------------------- Practice ----------------------
 /*
-print on the console numbers from 1 to 100.
-but do not use any number in your code
+    print on the console numbers from 1 to 100.
+    but do not use any number in your code
  */
 
     public static void main(String[] args) {
