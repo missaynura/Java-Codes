972691cdeec9190e5f94905be8fbb9a9811729f4
Aynura Cicek <126356820+missaynura@users.noreply.github.com>
2023-08-18 09:58:43 +0100

    version01

--- src/main/java/question_bank/QB_ScannerIncrement01.java
+++ src/main/java/question_bank/QB_ScannerIncrement01.java
@@ -4,28 +4,27 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class QB_ScannerIncrement01 {
-
-    /* QB ScannerIncrement - 5
-
-     * System.out.println('A'>'a' && 'c'<'f');
-
-     * When we compare char data types together in a condition to return boolean
-     * the conditions are compared based on their ASCII values
-     * Uppercase letters < LowerCase letters == So, A < a
-     * And alphabetical order go in Ascending order, so, A < Z or a < z
+    //---------------------- Scanner Increment ---------------------
+    /* 
+        QB ScannerIncrement - 5
+         * System.out.println('A'>'a' && 'c'<'f');
+    
+         * When we compare char data types together in a condition to return boolean
+         * the conditions are compared based on their ASCII values
+         * Uppercase letters < LowerCase letters == So, A < a
+         * And alphabetical order go in Ascending order, so, A < Z or a < z
 
      for ( char i = 'f'; i>'a'; i--){
         System.out.print(i + " " );
         i--;
     }
 
- * Whenever we print char => we get the actual value, NOT the ASCII value
- * But, when we do auto-boxing => we get the ASCII value
- * e.g.
+     * Whenever we print char => we get the actual value, NOT the ASCII value
+     * But, when we do auto-boxing => we get the ASCII value
+     * e.g.
 
     char letter = 'A';
     System.out.println( letter + 0 ); => converting the char to int
-
      */
 
         public static void main(String[] args) {
@@ -46,18 +45,17 @@ public class QB_ScannerIncrement01 {
             int result = 2 + 3 * (12 - 4 / 2) / 15 - 3;
             System.out.println(result);
 
-/**********************************************
-
- ScannerIncrement 7
- * Mathematical operations in Java work in specific order
- * Brackets => (12 - 4/2)
- *              => works left to right if it is opposite signs
- *              Java considers mathematical operations like in Math
- *              and works left to right
- *              First comes brackets => and then / and * and doesn't matter if change order and
- *              Similarly for + and -
- *
- */
+    /*
+         ScannerIncrement 7
+         * Mathematical operations in Java work in specific order
+         * Brackets => (12 - 4/2)
+         *              => works left to right if it is opposite signs
+         *              Java considers mathematical operations like in Math
+         *              and works left to right
+         *              First comes brackets => and then / and * and doesn't matter if change order and
+         *              Similarly for + and -
+         *
+     */
 
             System.out.println(5 / 2); // if numbers are both int => return int
             System.out.println(5 / 2.0); // autobox because int < double, therefore return == double
@@ -72,7 +70,6 @@ public class QB_ScannerIncrement01 {
             On the other hand, Scanner methods are non-static
             and require an object to be created first before they can be called.
          */
-
 
             int year = calculateAge(2000);
             System.out.println(year); // return value from the calculateAge() method
@@ -96,9 +93,7 @@ public class QB_ScannerIncrement01 {
              - create an object for the Test class to use the non-static variables/methods too
              }
              }
-
-
-             */
+           */
         }
 
         public static int calculateAge(int dateOfBirth) { // method is static => can use directly inside main
@@ -109,6 +104,4 @@ public class QB_ScannerIncrement01 {
 
             return LocalDate.now().getYear() - dateOfBirth;
         }
-
-
 }
